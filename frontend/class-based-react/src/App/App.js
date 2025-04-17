a403d1464a1227e6c817c252cf34342b12baf3a2
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-04-17 22:35:30 +0545

    fixed the last bad commit

--- frontend/class-based-react/src/App/App.js
+++ frontend/class-based-react/src/App/App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
         <div className="h-full w-full flex flex-col justify-top sm:gap-32 sm:items-center sm:bg-custom-blue/5">
           <div className="flex justify-start w-full">
             <Logo
-              path="logo-transparent.svg"
+              path="images/logo-transparent.svg"
               className="flex flex-col justify-center items-center w-24 sm:w-36 h-auto ml-[-11] mt-[-6] "
             />
           </div>
