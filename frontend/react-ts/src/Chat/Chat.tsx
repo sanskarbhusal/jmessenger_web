5097750864bfc55c1b80799aae661622e26dc3fe
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-05-12 01:51:57 +0545

    css

--- frontend/react-ts/src/Chat/Chat.tsx
+++ frontend/react-ts/src/Chat/Chat.tsx
@@ -6,7 +6,7 @@ type Props = RouteComponentProps;
 class Chat extends React.Component<Props> {
   render() {
     return (
-      <div className=" w-full h-full ">
+      <div className="h-dvh w-dvw bg-custom-blue">
         <NavBar />
       </div>
     );
