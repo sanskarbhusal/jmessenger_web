13d4228f7978cd3628f78dd9d484b17e6da25f9c
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-05-12 18:52:53 +0545

    working on chatlist

--- frontend/react-ts/src/Chat/ContactList/ContactList.tsx
+++ frontend/react-ts/src/Chat/ContactList/ContactList.tsx
@@ -3,9 +3,48 @@ import Contact from "./Contact";
 export default class Contacts extends React.Component {
   render() {
     const Contacts = () =>
-      ["", "", "", "", "", "", "", "", "", "", "", ""].map((item, index) => (
-        <Contact key={index + item} />
-      ));
+      [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ",",
+        ",",
+        ",",
+        ",",
+        ",",
+        ",",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+      ].map((item, index) => <Contact key={index + item} />);
     return (
       <div className="h-full w-full overflow-auto scrollbar-hidden">
         <div className="min-h-[48px]"></div>
--- frontend/react-ts/src/Chat/NavBar/NavBar.tsx
+++ frontend/react-ts/src/Chat/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import MenuButton from "./MenuButton";
 export default class NavBar extends React.Component {
   render() {
     return (
-      <div className="fixed h-fit w-full grid grid-cols-nav_bar grid-rows-nav_bar sm:bg-custom-blue/5 bg-white">
+      <div className="fixed h-fit w-full grid grid-cols-nav_bar grid-rows-nav_bar bg-white">
         <MenuButton />
         <SearchBar className="w-80 self-center" />
       </div>
