87012432436b586720b3587e6c5b9780e477f89f
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-17 00:07:15 +0545

    lakdf

--- frontend/src/Chat/NavBar/SearchBar/SearchBar.tsx
+++ frontend/src/Chat/NavBar/SearchBar/SearchBar.tsx
@@ -89,7 +89,7 @@ export default class SearchBar
       <label
         ref={this.inputReference}
         className={
-          "mr-[18px] border-[2px] min-h-[42px] border-solid rounded-full bg-gray-200/50" +
+          "mr-[9px] border-[2px] min-h-[42px] border-solid rounded-full bg-gray-200/50" +
           " " +
           "border-" +
           this.state.outerBorderColor
