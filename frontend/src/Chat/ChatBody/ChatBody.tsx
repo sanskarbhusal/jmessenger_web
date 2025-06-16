7fa6d8394d563e279b8e29c7f0dfb7bdec45aa10
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-16 17:18:47 +0545

    changed html structure of ChatBody

--- frontend/src/Chat/ChatBody/ChatBody.tsx
+++ frontend/src/Chat/ChatBody/ChatBody.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps, withRouter } from "react-router-dom";
 import ChatContext from "../ChatContext.tsx";
 import ChatTitle from "./ChatTitle";
 import ChatBubbleArea from "./ChatBubbleArea";
+import MessageBox from "./MessageBox";
 type Props = Required<typeof ChatBody.defaultProps> &
   RouteComponentProps & { className?: string };
 type State = {
@@ -25,14 +26,10 @@ class ChatBody extends React.Component<Props, State> {
         }
       >
         <div className="bg-blue-400 min-w-[388px] hidden sm:block"></div>
-        <div className="w-full grid grid-rows-[minmax(50px,70px)_1fr_minmax(50px,70px)]">
-          <div>
-            <ChatTitle />
-          </div>
-          <div className="bg-blue-300">
-            <ChatBubbleArea />
-          </div>
-          <div>Message Box</div>
+        <div className="w-full grid grid-rows-[minmax(56px,56px)_1fr_minmax(50px,70px)]">
+          <ChatTitle />
+          <ChatBubbleArea />
+          <MessageBox />
         </div>
       </div>
     );
