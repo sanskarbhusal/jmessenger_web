6a6bdaef8a4b72dff0d8546fcc895d33a331276a
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-17 00:05:38 +0545

    lakf

--- frontend/src/Chat/ChatBody/ChatTitle/ChatTitle.tsx
+++ frontend/src/Chat/ChatBody/ChatTitle/ChatTitle.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ChatTitleAvatar from "./ChatTitleAvatar";
+import { FiArrowLeft as ArrowLeft } from "react-icons/fi";
 type State = {};
 type Props = Required<typeof ChatTitle.defaultProps> & { className?: string };
 export default class ChatTitle extends React.Component<Props, State> {
@@ -7,7 +8,10 @@ export default class ChatTitle extends React.Component<Props, State> {
   state = {};
   render() {
     return (
-      <div className="w-full h-full border border-gray-300/80 border-l-[1px] border-r-0 border-t-0 border-b-0">
+      <div className="w-full h-full grid grid-cols-[60px_1fr] sm:flex border border-gray-300/80 border-l-[1px] border-r-0 border-t-0 border-b-0">
+        <div className="w-full h-full sm:hidden flex flex-row justify-center items-center">
+          <ArrowLeft className="w-[40px] h-[40px] p-[8px] text-gray-600/80 hover:bg-gray-500 rounded-full" />
+        </div>
         <ChatTitleAvatar />
       </div>
     );
