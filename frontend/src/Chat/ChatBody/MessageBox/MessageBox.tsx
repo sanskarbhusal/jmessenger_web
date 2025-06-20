9f22845c1085755cf1a0ba7d41985b374f5d98b0
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-21 02:51:01 +0545

    This is a autmated commit

--- frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
+++ frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
@@ -5,8 +5,8 @@ export default class MessageBox extends React.Component<Props, State> {
   static defaultProps = { foo: "foo" };
   state = {};
   render() {
-    return <div className={"w-full h-full flex flex-col justify-start items-center drop-shadow-lg" + " " + this.props.className}>
-      <div className="h-[50px] w-[90%] bg-white rounded-3xl border-[1px] border-gray-300/85">
+    return <div className={"w-full h-full flex flex-col justify-start items-center drop-shadow-xl" + " " + this.props.className}>
+      <div className="h-[50px] w-[90%] sm:w-[70%] bg-white rounded-3xl border-[1px] border-gray-300/85">
         <input placeholder="Message" className="h-full w-full pl-10 pr-10 text-lg font-normal font-sans bg-transparent placeholder-gray-500/80 outline-none"></input>
       </div>
     </div>;
