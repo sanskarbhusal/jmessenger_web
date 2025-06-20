439dd5dbf241fcec0ed7f3a9b52cf33121fe398c
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-20 18:12:34 +0545

    This is a autmated commit

--- frontend/src/Chat/ChatBody/ChatTitle/ChatTitle.tsx
+++ frontend/src/Chat/ChatBody/ChatTitle/ChatTitle.tsx
@@ -10,17 +10,14 @@ export default class ChatTitle extends React.Component<Props, State> {
   declare context: React.ContextType<typeof ChatContext>;
   render() {//border-gray-300/85
     return (
-      <div className="w-full h-full hidden sm:block">
-
-        <div className="w-full h-full grid grid-cols-[60px_1fr] sm:flex shadow-md">
-          <div className="w-full h-full sm:hidden flex flex-row justify-center items-center">
-            <ArrowLeft
-              onClick={() => this.context()}
-              className="w-[40px] h-[40px] p-[8px] text-gray-600/80 hover:bg-gray-100 rounded-full"
-            />
-          </div>
-          <ChatTitleAvatar />
+      <div className="w-full h-full grid grid-cols-[60px_1fr] sm:flex shadow-md">
+        <div className="w-full h-full sm:hidden flex flex-row justify-center items-center">
+          <ArrowLeft
+            onClick={() => this.context()}
+            className="w-[40px] h-[40px] p-[8px] text-gray-600/80 hover:bg-gray-100 rounded-full"
+          />
         </div>
+        <ChatTitleAvatar />
       </div>
     );
   }
--- frontend/src/Chat/ContactList/ContactList.tsx
+++ frontend/src/Chat/ContactList/ContactList.tsx
@@ -37,7 +37,7 @@ export default class ContactList extends React.Component<Props> {
     return (
       <div
         className={
-          "h-full w-full pl-5 pr-5 pt-[12px] pb-[12px] sm:pl-6 sm:pr-6 sm:pt-[6px] sm:pb-[12px] gap-[12px] border-gray-300/85 border-r-[1px] flex flex-col items-center 2xl:bg-transparent overflow-y-scroll overflow-x-hidden scrollbar-thin drop-shadow-sm" +
+          "h-full w-full p-[12px] sm:pl-6 sm:pr-6 sm:pt-[6px] sm:pb-[12px] gap-[12px] border-gray-300/85 border-r-[1px] flex flex-col items-center 2xl:bg-transparent overflow-y-scroll overflow-x-hidden scrollbar-thin drop-shadow-sm" +
           " " +
           this.props.className
         }
