72c36992601460cee3a9caaf711a63e15fdc5df2
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-22 01:53:55 +0545

    This is a autmated commit

--- frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
+++ frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
@@ -7,12 +7,12 @@ export default class MessageBox extends React.Component<Props, State> {
   render() {
     return <div className={"w-full h-full flex flex-row justify-center items-start sm:items-center sm:bg-custom-blue/10" + " " + this.props.className}>
       <div className="h-full w-full flex flex-row justify-center items-center">
-        <textarea
+        <input
           autoComplete="off"
           spellCheck="false"
           placeholder="Message"
-          className="relative overflow-auto sm:bottom-[6px] w-[90%] sm:w-[96%] min-h-[50px] sm:h-[52px] text-lg text-black font-medium font-sans rounded-3xl border-[1px] border-gray-300 focus:border-gray-400 bg-white drop-shadow-sm shadow-inner pl-10 pr-10 placeholder-gray-500/80 outline-none">
-        </textarea>
+          className="relative sm:bottom-[6px] w-[90%] sm:w-[96%] min-h-[50px] sm:h-[52px] text-lg text-black font-medium font-sans rounded-3xl border-[1px] border-gray-300 focus:border-gray-400 bg-white drop-shadow-sm shadow-inner pl-10 pr-10 placeholder-gray-500/80 outline-none">
+        </input>
       </div>
     </div>;
   }
