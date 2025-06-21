f5b70290adbcc0184e735a2ca39a7cd87c02d6f4
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-21 18:07:44 +0545

    This is a autmated commit

--- frontend/src/Chat/ChatBody/ChatBubbleArea/ChatBubbleArea.tsx
+++ frontend/src/Chat/ChatBody/ChatBubbleArea/ChatBubbleArea.tsx
@@ -10,9 +10,9 @@ export default class ChatBubbleArea extends React.Component<Props, State> {
   render() {
     return (
       <div className="relative w-full h-full flex flex-row justify-center items-center">
-        <div className="blur-md rounded-3xl absolute z-0 w-[97%] h-[730px] bg-custom-blue/40"></div>
-        <div className="relative z-10 bg-white w-[97%] h-[728px] rounded-3xl">
-          <div className={"relative z-20 w-full h-full bg-white rounded-3xl " + " " + this.props.className}>
+        <div className="hidden sm:block blur-sm rounded-3xl absolute z-0 w-[90%] h-[96%] bg-custom-blue/20"></div>
+        <div className="hidden sm:block relative z-10 bg-white w-[90%] h-[96%] rounded-3xl">
+          <div className={"relative z-20 w-full h-full bg-custom-blue/10 rounded-3xl border border-custom-blue/15 " + " " + this.props.className}>
             <ChatBubble />
           </div>
         </div>
