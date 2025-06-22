aed32182a20ea3bd3b398927326c2322f7949167
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-22 13:51:04 +0545

    This is a autmated commit

--- frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
+++ frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
@@ -6,13 +6,14 @@ export default class MessageBox extends React.Component<Props, State> {
   state = {};
   render() {
     return <div className={"w-full h-full flex flex-row justify-center items-start sm:items-center sm:bg-custom-blue/10" + " " + this.props.className}>
-      <div className="h-full w-full flex flex-row justify-center items-center">
+      <div className="relative h-full w-full flex flex-row justify-center items-center">
         <input
           autoComplete="off"
           spellCheck="false"
           placeholder="Message"
-          className="relative sm:bottom-[6px] w-[90%] sm:w-[96%] min-h-[50px] sm:h-[52px] text-lg text-black font-medium font-sans rounded-3xl border-[1px] border-gray-300 focus:border-gray-400 bg-white drop-shadow-sm shadow-inner pl-10 pr-10 placeholder-gray-500/80  outline-none ring-0 focus:ring-0 focus:outline-none">
+          className="peer relative z-10 sm:bottom-[6px] w-[90%] sm:w-[96%] min-h-[50px] sm:h-[52px] text-lg text-black font-medium font-sans rounded-3xl border-[1px] border-gray-300 focus:border-gray-400 sm:border-transparent bg-white  drop-shadow-sm sm:drop-shadow-none shadow-inner pl-10 pr-10 placeholder-gray-500/80  outline-none ring-0 focus:ring-0 focus:outline-none">
         </input>
+        <div className="hidden peer-focus:block sm:block absolute z-0 bottom-[24px] h-[55px] w-[96%] blur-sm rounded-3xl bg-custom-blue/10"></div>
       </div>
     </div>;
   }
