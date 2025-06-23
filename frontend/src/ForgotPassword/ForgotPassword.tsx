20d0e386ccefef3e9c6a65f7465f9521377a45e5
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-24 01:24:47 +0545

    This is a autmated commit

--- frontend/src/ForgotPassword/ForgotPassword.tsx
+++ frontend/src/ForgotPassword/ForgotPassword.tsx
@@ -4,14 +4,14 @@ class ForgotPassword extends React.Component<Props> {
   render() {
     return (
       <div className="h-full w-full flex items-center sm:justify-center sm:bg-custom-blue/10">
-        <div className="w-fit h-fit bg-white">
-          <div className="sm:justify-self-center self-center col-span-3 w-[77%] h-[550px] sm:h-fit sm:w-96 font-sans sm:bg-custom-blue/5 flex flex-col gap-5 justify-center items-left sm:items-center sm:rounded-lg pl-[6%] ml-[6%] sm:m-0 sm:p-8 pt-0  sm:border-custom-blue-dark/5 sm:border-1 sm:shadow-2xl sm:shadow-custom-blue/20 border border-solid border-l-1 border-b-0 border-t-0 border-r-0 border-custom-blue">
+        <div className="w-fit h-fit bg-white sm:rounded-3xl">
+          <div className="sm:justify-self-center self-center col-span-3 w-[77vw] h-[550px] sm:h-fit sm:w-96 font-sans sm:bg-custom-blue/5 flex flex-col gap-5 justify-center items-left sm:items-center sm:rounded-3xl pl-[6%] ml-[6%] sm:m-0 sm:p-8 pt-0  sm:border-custom-blue-dark/5 sm:border-1 sm:shadow-2xl sm:shadow-custom-blue/20 border border-solid border-l-1 border-b-0 border-t-0 border-r-0 border-custom-blue">
             <div className="text-2xl font-semibold mt-[-8px]">
               Request new password
             </div>
 
             <div className="flex flex-col w-full">
-              <label htmlFor="pass" className="mb-2 ">
+              <label htmlFor="pass" className="mb-2">
                 New password
               </label>
               <input
