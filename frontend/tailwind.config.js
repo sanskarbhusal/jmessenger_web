40a71b2563374054f8f4222ec94e43c2bf8cd5ae
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-23 19:22:23 +0545

    This is a autmated commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        "custom-blue-dark": "darkblue",
-        "custom-blue": "purple",//"#0284C7"
+        "custom-blue-dark": "darkgreen",
+        "custom-blue": "green",//"#0284C7"
       },
       gridTemplateColumns: {
         search_bar: "49px 1fr 49px",
