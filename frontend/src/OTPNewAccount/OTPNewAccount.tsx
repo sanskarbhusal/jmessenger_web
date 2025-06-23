3f4c3f36aa6ece786cf276af94867eb128422cfc
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-24 02:03:12 +0545

    This is a autmated commit

--- frontend/src/OTPNewAccount/OTPNewAccount.tsx
+++ frontend/src/OTPNewAccount/OTPNewAccount.tsx
@@ -9,7 +9,7 @@ class OTPNewPassword extends React.Component<Props> {
       <div className="h-full w-full flex items-center sm:justify-center sm:bg-custom-blue/10">
         <div className="w-fit h-fit sm:bg-white rounded-3xl">
 
-          <div className="w-[77%] h-[550px] sm:h-fit sm:w-fit font-sans sm:bg-custom-blue/5 flex flex-col gap-5 justify-center items-left sm:items-center sm:rounded-3xl pl-[6%] ml-[6%] sm:m-0 sm:p-8 pt-0  sm:border-custom-blue-dark/5 sm:border-1 sm:shadow-2xl sm:shadow-custom-blue/20 border border-solid border-l-1 border-b-0 border-t-0 border-r-0 border-custom-blue">
+          <div className="w-[77%] h-[550px] sm:h-fit sm:w-fit font-sans sm:bg-custom-blue/5 flex flex-col gap-5 justify-center items-left sm:items-center sm:rounded-3xl pl-[6%] ml-[6%] sm:m-0 sm:p-8 pt-0 sm:border-custom-blue-dark/5 sm:border-1 sm:shadow-2xl sm:shadow-custom-blue/20 border border-solid border-l-1 border-b-0 border-t-0 border-r-0 border-custom-blue">
             <div className="text-2xl font-semibold mt-[-8px]">
               Email verification
             </div>
@@ -29,11 +29,11 @@ class OTPNewPassword extends React.Component<Props> {
                   type="number"
                   id="otp"
                   placeholder="6-digit number"
-                  className="w-[190px] no-spinner font-sans text-base p-2 text-center sm:rounded-3xl border border-solid sm:bg-transparent border-gray-300 focus:outline-custom-blue"
+                  className="w-[202px] no-spinner font-sans text-base p-2 sm:text-center sm:rounded-3xl border border-solid sm:bg-transparent border-gray-300 focus:outline-custom-blue"
                 />
               </div>
             </div>
-            <div className="w-[190px] flex flex-row justify-left sm:justify-center mb-2">
+            <div className="w-[202px] flex flex-row justify-left sm:justify-center mb-2">
               <button
                 onClick={this.onClickVerify}
                 className=" w-full font-sans text-base active:bg-custom-blue-dark bg-custom-blue border-0 text-white h-10 sm:rounded-3xl font-semibold "
--- frontend/src/OTPNewPassword/OTPNewPassword.tsx
+++ frontend/src/OTPNewPassword/OTPNewPassword.tsx
@@ -9,7 +9,7 @@ class EmailVerification extends React.Component<Props> {
       <div className="h-full w-full flex items-center sm:justify-center sm:bg-custom-blue/10">
         <div className="w-fit h-fit sm:bg-white sm:rounded-3xl">
 
-          <div className="w-[77%] h-[550px] sm:h-fit sm:w-fit font-sans sm:bg-custom-blue/5 flex flex-col gap-5 justify-center items-left sm:items-center sm:rounded-3xl pl-[6%] ml-[6%] sm:m-0 sm:p-8 pt-0  sm:border-custom-blue-dark/5 sm:border-1 sm:shadow-2xl sm:shadow-custom-blue/20 border border-solid border-l-1 border-b-0 border-t-0 border-r-0 border-custom-blue">
+          <div className="w-[77%] h-[550px] sm:h-fit sm:w-fit font-sans sm:bg-custom-blue/5 flex flex-col gap-5 justify-center items-left sm:items-center sm:rounded-3xl pl-[6%] ml-[6%] sm:m-0 sm:p-8 pt-0 sm:border-custom-blue-dark/5 sm:border-1 sm:shadow-2xl sm:shadow-custom-blue/20 border border-solid border-l-1 border-b-0 border-t-0 border-r-0 border-custom-blue">
             <div className="text-2xl font-semibold mt-[-8px]">
               Authorize change
             </div>
@@ -29,14 +29,14 @@ class EmailVerification extends React.Component<Props> {
                   type="number"
                   id="otp"
                   placeholder="6-digit number"
-                  className="w-[202px] font-sans text-base p-2 sm:rounded-lg border border-solid sm:bg-transparent border-gray-300 focus:outline-custom-blue"
+                  className="w-[202px] font-sans text-base sm:text-center p-2 sm:rounded-3xl border border-solid sm:bg-transparent border-gray-300 focus:outline-custom-blue"
                 ></input>
               </div>
             </div>
             <div className="w-[202px] flex flex-row justify-left sm:justify-center mb-2">
               <button
                 onClick={this.onClickVerifyOTP}
-                className=" w-full font-sans text-base active:bg-custom-blue-dark bg-custom-blue border-0 text-white h-10 sm:rounded-lg font-semibold "
+                className=" w-full font-sans text-base active:bg-custom-blue-dark bg-custom-blue border-0 text-white h-10 sm:rounded-3xl font-semibold "
               >
                 Verify OTP
               </button>
