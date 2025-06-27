cf7090969104c7bd560380bc4b07a2309746838a
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-27 11:27:34 +0545

    This is an autmated commit

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -29,23 +29,21 @@ export default class App extends React.Component {
   state = { isDesktop: false }
 
   componentDidMount() {
-    const width = this.appRef.current!.offsetWidth
-    if (width <= 640) {
-      this.setState({ isDesktop: false })
-    } else {
-      this.setState({ isDesktop: true })
-    }
+    const width = this.appRef.current?.offsetWidth
+    this.setState({ isDesktop: width! > 640 ? true : false })
   }
-
   render() {
     return (
       <Router>
         <div ref={this.appRef} className="fixed h-full w-full overflow-none">
-          <div className="h-full flex flex-col justify-center items-center sm:hidden font-black text-2xl gap-2">
-            Please visit the site from desktop.
-            <div>
-              Sorry for the inconvenience
-            </div>
+          <div className="h-full flex flex-col justify-center items-center sm:hidden font-extrabold text-2xl gap-2 bg-custom-blue/10 text-custom-blue-dark">
+            <span>
+              Please visit the site from your laptop/desktop.
+            </span>
+            <span>
+              Sorry for the inconvenience.
+            </span>
+
 
           </div>
           {this.state.isDesktop && <Routes />}
