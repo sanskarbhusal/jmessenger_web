45bd39835012b562c8095078e242975a41dbc959
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-29 01:37:46 +0545

    This is an autmated commit

--- frontend/src/Chat/ChatBody/ChatBubbleArea/ChatBubble/ChatBubble.tsx
+++ frontend/src/Chat/ChatBody/ChatBubbleArea/ChatBubble/ChatBubble.tsx
@@ -41,7 +41,9 @@ export default class ChatBubble extends React.Component<Props, State> {
     return (
       <div className="sm:w-[45%] group sm:self-end sm:mt-10 sm:mb-10 sm:mr-10 sm:bg-white sm:rounded-xl">
         <div className="w-full h-full flex flex-col bg-custom-blue/10 sm:border border-custom-blue/30 rounded-xl selection:bg-custom-blue/90 selection:text-white">
-          <div className="pl-3 pt-2 w-full h-full flex font-normal text-base rounded-xl text-pretty ">Web frontend is so easy. Lots of complicated graphics programming and data encoding/decoding is abstrated and automated. Like for example this emoji works out of the box 😆. You just copy the emojy from any platfrom and the emoji data is decoded and rendered properly in the browser. You need not be a programming wizard to be able to show emojis! </div>
+          <div className="pl-3 pt-2 w-full h-full flex font-normal text-base rounded-xl text-pretty ">
+            Web frontend is so easy. Lots of complicated UI programming and character encoding/decoding is abstrated. Like for example this 😆 emoji works out of the box. You just copy the emoji from any platfrom and the emoji data is decoded and rendered properly in the browser. You need not be a programming wizard to be able to show emojis.
+          </div>
           <div className="flex flex-row justify-end items-end gap-1 pr-1">
             <div className="flex flex-row items-center font-medium text-xs text-gray-500 select-none">{time}</div>
             <DoubleCheckIcon className="text-custom-blue/90 sm:group-hover:text-gray-400 font-bold text-md h-fit w-fit" />
--- frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
+++ frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
@@ -14,7 +14,7 @@ export default class MessageBox extends React.Component<Props, State> {
           placeholder="Type a message"
           className="peer field-sizing-content overflow-hidden relative z-10 pt-[9px] sm:bottom-[6px] w-[90%] sm:w-[96%] sm:min-h-[52px] sm:max-h-[200px] overflow-y-scroll scrollbar-thin font-normal text-lg text-black  font-sans rounded-3xl border-[1px] border-gray-300 bg-white sm:bg-gray-100 sm:drop-shadow-none shadow-inner pl-10 pr-10 placeholder-gray-500/80 outline-none ring-0 focus:ring-0 focus:outline-none resize-none">
         </textarea>
-        <div className="hidden transition sm:block sm:peer-focus:hidden  absolute z-0 bottom-[19px] h-[71%] w-[97%] blur-sm rounded-3xl bg-custom-blue/30 sm:peer-focus:shadow-inner sm:peer-focus:drop-shadow-lg  "></div>
+        <div className="hidden transition sm:block sm:peer-focus:hidden  absolute z-0 bottom-[19px] h-[71%] w-[97%] blur-sm rounded-3xl bg-custom-blue-dark/30 sm:peer-focus:shadow-inner sm:peer-focus:drop-shadow-lg  "></div>
       </div>
     );
   }
