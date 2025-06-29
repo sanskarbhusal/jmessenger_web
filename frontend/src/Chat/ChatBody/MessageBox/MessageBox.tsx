a0de42e5bd336f383064c094a6128350bbda7a92
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-06-29 14:11:16 +0545

    This is an autmated commit

--- frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
+++ frontend/src/Chat/ChatBody/MessageBox/MessageBox.tsx
@@ -13,11 +13,11 @@ export default class MessageBox extends React.Component<Props, State> {
           autoComplete="off"
           spellCheck="false"
           placeholder="Type a message"
-          className="peer field-sizing-content overflow-hidden pt-[11px] w-[92%] sm:min-w-[92%] sm:max-w-[92%] sm:min-h-[54px] sm:max-h-[200px] overflow-y-scroll scrollbar-thin font-normal text-lg text-black  font-sans rounded-3xl rounded-r-none border-[1px] border-custom-blue/45 border-r-0 bg-white sm:bg-gray-100 pl-10 pr-10 placeholder-gray-500/80 outline-none ring-0 focus:ring-0 focus:outline-none resize-none">
+          className="peer field-sizing-content overflow-hidden pt-[11px] w-[92%] sm:min-w-[92%] sm:max-w-[92%] sm:min-h-[54px] sm:max-h-[200px] overflow-y-scroll scrollbar-thin font-normal text-lg text-black  font-sans rounded-3xl rounded-r-none border-[1px] border-custom-blue/45 border-r-0 bg-white sm:bg-gray-100 pl-10 pr-10 placeholder-gray-500/80 outline-none ring-0 focus:ring-0 focus:outline-none resize-none selection:bg-custom-blue/90 selection:text-white">
         </textarea>
         <div className="flex flex-row justify-center items-center h-[54px] w-[52px] border-[1px] border-l-0 border-custom-blue/45 bg-gray-100 rounded-r-3xl">
-          <div className="relative z-0 group p-[6px] flex flex-row justify-center items-center rounded-full bg-custom-blue/10 sm:border-[1px] sm:border-custom-blue hover:border-gray-100 hover:bg-gray-100 transition">
-            <SendButton className="h-[28px] w-[28px] text-custom-blue transition" />
+          <div className="group relative z-0 group p-[6px] flex flex-row justify-center items-center rounded-full sm:border-[1px] sm:border-transparent bg-custom-blue/20 hover:border-custom-blue/45 active:bg-custom-blue transition">
+            <SendButton className="h-[29px] w-[29px] text-custom-blue group-active:text-white transition duration-75" />
           </div>
         </div>
       </div>
--- frontend/src/Chat/ContactList/Contact/Contact.tsx
+++ frontend/src/Chat/ContactList/Contact/Contact.tsx
@@ -33,9 +33,9 @@ export default class Contact extends React.Component<Props, State> {
     return (
       <div
         onClick={this.handleClick}
-        className="group sm:transition ease-out w-full h-fit rounded-full hover:drop-shadow-lg bg-white sm:bg-transparent hover:bg-custom-blue/10 hover:shadow-md active:bg-custom-blue/30 font-sans border border-gray-300/85 sm:border-transparent flex flex-row items-center select-none"
+        className="group sm:transition delay-0 linear w-full h-fit rounded-full hover:drop-shadow-lg bg-white sm:bg-transparent hover:bg-custom-blue/10 hover:shadow-md active:bg-custom-blue/30 font-sans border border-gray-300/85 sm:border-transparent flex flex-row items-center select-none"
       >
-        <div className="sm:transition min-h-[60px] min-w-[60px] flex flex-row justify-center items-center bg-white sm:bg-transparent rounded-full shadow-md sm:group-hover:shadow-none" >
+        <div className="sm:transition delay-0 linear min-h-[60px] min-w-[60px] flex flex-row justify-center items-center bg-white sm:bg-transparent rounded-full shadow-md sm:group-hover:shadow-none" >
 
           <div className="group min-h-[48px] min-w-[48px] sm:group-hover:min-w-[58px] sm:group-hover:min-h-[58px] bg-gradient-to-b from-orange-400/90 to-orange-500/90 flex justify-center items-center rounded-full">
             <div className="font-sans font-extrabold text-3xl text- group-hover:text-4xl text-white">S</div>
