50f11fbe91fb908639319226fdecadb8dc0eb07b
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-07-22 00:35:24 +0545

    automated commit for chat_server branch

--- auth_server/file.js
+++ auth_server/file.js
@@ -1,12 +1,20 @@
 import express from "express"
 import cors from "cors"
 const app = express()
-app.listen(8080, () => console.log("Listening on port 80"))
+
 const corsOption = {
-    origin: ""
+    origin: "http://localhost:8080",
+    credentials: true,
+    optionsSuccessStatus: 200
 }
 app.use(cors(corsOption))
+app.use(express.text())
+app.listen(8080, () => console.log("Listening on port 8080"))
+
+app.get("/", (req, res) => { res.sendFile("/home/sanskar/jmessenger_web/auth_server/index.html") })
+
 app.post("/code", (req, res) => {
+
     console.log(req.body)
-    res.send("Success")
+
 })
