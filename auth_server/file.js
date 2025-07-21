c23b951955644745c5d0d9207505e71e6ddf21db
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-07-21 17:46:20 +0545

    commited from backend

--- auth_server/file.js
+++ auth_server/file.js
@@ -1,6 +1,12 @@
 import express from "express"
+import cors from "cors"
 const app = express()
 app.listen(8080, () => console.log("Listening on port 80"))
+const corsOption = {
+    origin: ""
+}
+app.use(cors(corsOption))
 app.post("/code", (req, res) => {
     console.log(req.body)
+    res.send("Success")
 })
