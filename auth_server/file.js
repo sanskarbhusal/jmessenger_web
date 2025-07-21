a12642324502cb0553f1fc48d40d742809bee68c
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-07-21 17:53:19 +0545

    commited from backend

--- auth_server/file.js
+++ auth_server/file.js
@@ -1,6 +1,12 @@
 import express from "express"
+
 const app = express()
+
 app.listen(8080, () => console.log("Listening on port 80"))
+
+app.get("/", (req, res) => { res.sendFile(__dirname + "/index.html") })
+
 app.post("/code", (req, res) => {
     console.log(req.body)
+    res.send("Success")
 })
